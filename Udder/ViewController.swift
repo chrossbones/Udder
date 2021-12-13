16514e695a5b2ab1120f48541bdfd5a0172e5d5b
Christian Flores <khris_is@icloud.com>
2021-12-13 06:21:43 -0600

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Christian Flores Salgado on 13/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
